e16b1354ef238cc6577fb305dc868f92c804a4ee
krvaghani1 <kaushik.vaghani111@gmail.com>
2019-08-09 14:02:45 +0530

    first commit

---
+++ SimpleRestApisApplication/src/main/java/com/example/simplerestapis/SimpleRestApisApplication.java
@@ -0,0 +1,14 @@
+package com.example.simplerestapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestApisApplication.class, args);
+	    System.out.println("API is Running...");
+	}
+
+}
---
+++ SimpleRestApisApplication/src/test/java/com/example/simplerestapis/SimpleRestApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplerestapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
